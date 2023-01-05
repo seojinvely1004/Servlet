98f982e9762e57138866c5408a703910f0c80297
seojinvely1004 <seojin0124@naver.com>
2023-01-05 15:24:46 +0900

    refact(servlet) : 로그인 시 마이페이지 보여줄 수 있도록 수정.

--- servlettest/src/main/java/session/MyPageServlet.java
+++ servlettest/src/main/java/session/MyPageServlet.java
@@ -15,11 +15,17 @@ public class MyPageServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//로그인 한 이후이므로 새로운세션이 아니다. (기존세션 가져와서 사용)
 		HttpSession session = request.getSession();
-		//로그인했던 id 공유하여 사용한다
-		String sessionid = (String)session.getAttribute("sessionid");//정보를얻어오는것이므로 getAttribute
-		//jdbc빼고실습한다 (세션에집중)
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
-		out.println("<h1>" + sessionid + "님의 회원정보 페이지입니다.</h1>");
+		//로그인했던 id 공유하여 사용한다
+		if(session.getAttribute("sessionid") != null) {
+			out.println("<h1>" + session + "님의 회원정보 페이지입니다.</h1>");
+		}else {
+			out.println("<h1>로그인 먼저 하세요. 그래야 회원정보를 보여줍니다.</h1>");
+		}
+		String sessionid = (String)session.getAttribute("sessionid");//정보를얻어오는것이므로 getAttribute
+		//jdbc빼고실습한다 (세션에집중)
+		
+		
 		}
 }
