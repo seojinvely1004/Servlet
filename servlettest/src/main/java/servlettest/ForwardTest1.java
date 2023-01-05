7f05fa6181ac2e2f20a80272713faa479d8b6d53
seojinvely1004 <seojin0124@naver.com>
2023-01-05 11:04:50 +0900

    feat(servlet):forward1을실행하여 forward2 (같은정보활용하기)

--- servlettest/src/main/java/servlettest/ForwardTest1.java
+++ servlettest/src/main/java/servlettest/ForwardTest1.java
@@ -1,8 +1,8 @@
 package servlettest;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,9 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 public class ForwardTest1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");//사용자에게 파라미터(id)하나를 전달받는다
-		response.setContentType("text/html;charset=utf-8");
+		request.setAttribute("upperid", id.toUpperCase());
+		
+		/*response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
-		out.println(id + " 회원님 반갑습니다.");
+		out.println("<h1>" + request.getAttribute("upperid") + " 회원님 반갑습니다.</h1>");
+		*/
+		//out.println("<h1>" + id + " 회원님 반갑습니다.</h1>");
+		//"웹어플리케이션명포함/"
+		RequestDispatcher rd = request.getRequestDispatcher("/forward2");
+		rd.forward(request, response);
 	}
-
 }
